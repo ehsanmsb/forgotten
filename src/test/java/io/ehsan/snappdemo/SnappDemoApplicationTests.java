d986e7d0f9bdbc4cc43a424273b3bd43cebcde20
Ehsan Mosayebi <39300504+ehsanmsb@users.noreply.github.com>
2023-11-15 03:17:43 -0800

    fix: test

--- src/test/java/io/ehsan/snappdemo/SnappDemoApplicationTests.java
+++ src/test/java/io/ehsan/snappdemo/SnappDemoApplicationTests.java
@@ -1,80 +1,10 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.request.async.DeferredResult;
+FROM registry.devopsco.space:5000/bellsoft/liberica-openjdk-alpine:17
 
-import java.util.concurrent.CompletableFuture;
+WORKDIR /app
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+EXPOSE 8080
 
-public class DefaultControllerTest {
+ARG JAR_FILE
+COPY ${JAR_FILE} app.jar
 
-    private DefaultController defaultController;
-	private CustomFlagHealthIndicator customFlagHealthIndicator;
-
-    @Mock
-    private CustomFlagHealthIndicator customFlagHealthIndicator;
-
-    @BeforeEach
-    public void setUp() {
-        defaultController = new DefaultController(customFlagHealthIndicator);
-    }
-
-	@BeforeEach
-    public void setUp() {
-        customFlagHealthIndicator = new CustomFlagHealthIndicator();
-    }
-
-    @Test
-    public void testChangeHealthStatus() {
-        ResponseEntity<Void> expectedResponse = ResponseEntity.ok(null);
-        when(customFlagHealthIndicator.setEverythingIsOk(true)).thenReturn(true);
-
-        ResponseEntity<Void> response = defaultController.changeHealthStatus(true);
-
-        assertEquals(expectedResponse, response);
-        verify(customFlagHealthIndicator).setEverythingIsOk(true);
-    }
-
-    @Test
-    public void testIncreaseMemory() {
-        int megabytes = 2;
-        int sleep = 5;
-        String expectedResponse = "2 MB of memory allocated for 5 s.";
-
-        ResponseEntity<String> response = defaultController.increaseMemory(megabytes, sleep);
-
-        assertEquals(expectedResponse, response.getBody());
-    }
-
-    @Test
-    public void testHealthIndicatorUp() {
-        Health health = customFlagHealthIndicator.health();
-        assertEquals(HealthStatus.UP, health.getStatus());
-    }
-
-    @Test
-    public void testHealthIndicatorDown() {
-        customFlagHealthIndicator.setEverythingIsOk(false);
-
-        Health health = customFlagHealthIndicator.health();
-        assertEquals(HealthStatus.DOWN, health.getStatus());
-    }
-
-    @Test
-    public void testSetEverythingIsOk() {
-        customFlagHealthIndicator.setEverythingIsOk(false);
-
-        Health health = customFlagHealthIndicator.health();
-        assertEquals(HealthStatus.DOWN, health.getStatus());
-
-        customFlagHealthIndicator.setEverythingIsOk(true);
-
-        health = customFlagHealthIndicator.health();
-        assertEquals(HealthStatus.UP, health.getStatus());
-    }
-}
+ENTRYPOINT ["java", "-XX:MaxRAMPercentage=90.0", "-jar", "app.jar"]
